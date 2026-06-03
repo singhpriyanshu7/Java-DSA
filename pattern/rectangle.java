301a7c93e5e722024196f9c4ca1292cbfe3f5ebf
Priyansh Singh <priyanshusingh55809@gmail.com>
2026-06-03 21:10:05 +0530

    pattern ~ rectangle

---
+++ pattern/rectangle.java
@@ -0,0 +1,15 @@
+public class rectangle {
+    public static void main (String args[]){
+        int n=5; //wos
+        int m =5; //column
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+System.out.print("*");
+
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
